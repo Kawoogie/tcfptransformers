3755d0c8c45cb2fa50fda40b769b8b8f23ab4c48
Kawoogie <85772341+Kawoogie@users.noreply.github.com>
2025-09-02 13:56:41 -0400

    Update mapcal.py
    Added custom exception for no column name found.

--- mapcal.py
+++ mapcal.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+from .exceptions import ColumnNotFound
 """
 This transformer computes the mean arterial pressure using the systolic and 
 diastolic information.
@@ -22,7 +22,11 @@ class MAPCalculation(BaseEstimator, TransformerMixin):
             new_column = "map"
             X_trans[new_column] = X_trans[self.dia] + ((1/3) * (X_trans[self.sys] - X_trans[self.dia]))
 
-        return X_trans
+            return X_trans
+        else:
+            raise ColumnNotFound(f"Either {self.dia} or {self.sys} not found "
+                                 f"using MAPCalculation transformer. Unable to "
+                                 f"compute MAP")
 
 
 def main() -> None:
